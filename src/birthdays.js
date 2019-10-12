f0f60c7eae76a8b6f48569e3111bdffd46649f6f
Gabriel <gabriel.cmartins11@gmail.com>
2019-10-12 20:48:20 -0300

    Javascript its broken, but its okay

--- src/birthdays.js
+++ src/birthdays.js
@@ -1,4 +1,5 @@
-let birthdays = []
+var birthdays = []
+let arr = []
 
 loadBirthdays()
 
@@ -6,22 +7,26 @@ function getBirthday() {
     let name = document.getElementById('birthday-name').value
     let date = document.getElementById('birthday-date').value
 
-    let dd = date.substr(8, 10)
-    let mm = date.slice(5, 7)
-    let yyyy = date.slice(0, 4)
+    if (name === '' || date === '') { return }
 
-    console.log(dd)
-    console.log(mm)
-    console.log(yyyy)
+    // let dd = date.substr(8, 10)
+    // let mm = date.slice(5, 7)
+    // let yyyy = date.slice(0, 4)
+
 
     saveBirthday(name, date)
     displayUpcomingBirthdays(name, date)
-    displayAllBirthdays(name, date, --birthdays.length)
+    displayAllBirthdays(name, date, birthdays.length)
 }
 
 function saveBirthday(name, date) {
-    birthdays.push({ name, date })
-    chrome.storage.sync.set({ birthdays: birthdays }, function () {
+    chrome.storage.sync.get(['birthdays'], function (result) {
+        let arr = result.birthdays === undefined ? [] : result.birthdays
+        arr.push({ name, date })
+        birthdays = arr
+        chrome.storage.sync.set({ birthdays: arr }, function () {
+        });
+
     });
 }
 
@@ -29,9 +34,24 @@ function deleteBirthday(index) {
     birthdays.splice(index, 1)
     chrome.storage.sync.set({ birthdays: birthdays }, function () {
     });
+    location.reload();
 }
 
+let differenceInDaysSetting = ''
+
 function loadBirthdays() {
+    chrome.storage.sync.get(['differenceInDaysSetting'], function (result) {
+        if (result.differenceInDaysSetting === '') {
+            chrome.storage.sync.set({ differenceInDaysSetting: 7 }, function () {
+                differenceInDaysSetting = 7
+            });
+        } else {
+            differenceInDaysSetting = result.differenceInDaysSetting
+        }
+        let DifferenceInDays = document.getElementById('difference-in-days')
+        DifferenceInDays.value = differenceInDaysSetting
+    });
+
     chrome.storage.sync.get(['birthdays'], function (result) {
         result.birthdays.forEach((element, index) => {
             displayUpcomingBirthdays(element.name, element.date)
@@ -42,9 +62,14 @@ function loadBirthdays() {
 
 }
 
+function setDifferenceInDays(num) {
+    chrome.storage.sync.set({ differenceInDaysSetting: num }, function () {
+        differenceInDaysSetting = num
+    });
+}
+
 function displayUpcomingBirthdays(name, date) {
 
-    let differenceInDaysSeting = 1
 
     let date1 = new Date(getCurrentDate())
     let date2 = new Date(date)
@@ -52,8 +77,12 @@ function displayUpcomingBirthdays(name, date) {
     let DifferenceInTime = date2.getTime() - date1.getTime();
     let DifferenceInDays = DifferenceInTime / (1000 * 3600 * 24);
 
-    if (DifferenceInDays >= 0 && DifferenceInDays <= differenceInDaysSeting) {
-        let birthday = document.createTextNode(`${name} - ${date}`)
+    if (DifferenceInDays >= 0 && DifferenceInDays <= differenceInDaysSetting) {
+        let newDate = date.replace(/-/g, "/")
+        let dd = newDate.substr(8, 10)
+        let mm = newDate.slice(5, 7)
+        let yyyy = newDate.slice(0, 4)
+        let birthday = document.createTextNode(`${name} - ${newDate}`)
         let item = document.createElement('li')
         item.appendChild(birthday)
         item.className = 'birthdays-item'
@@ -62,11 +91,23 @@ function displayUpcomingBirthdays(name, date) {
 }
 
 function displayAllBirthdays(name, date, index) {
-    let birthday = document.createTextNode(`${name} - ${date} - ${index}`)
+    let newDate = date.replace(/-/g, "/")
+    let dd = newDate.substr(8, 10)
+    let mm = newDate.slice(5, 7)
+    let yyyy = newDate.slice(0, 4)
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.setAttribute('id', index)
+    deleteBtn.setAttribute('class', 'birthday-btn delete-btn')
+    deleteBtn.innerHTML = 'Delete'
+    deleteBtn.addEventListener("click", function () { deleteBirthday(index) })
+    let birthday = document.createTextNode(`${name} ${newDate} - ${index}`)
     let item = document.createElement('li')
     item.appendChild(birthday)
+    item.appendChild(deleteBtn)
     item.className = 'birthdays-item'
     document.getElementById('all-birthdays').appendChild(item)
+
 }
 
 function getCurrentDate() {
@@ -98,13 +139,21 @@ function windowOnClick(event) {
     }
 }
 
+
 // Open modal
 let modal = document.querySelector(".modal");
-document.getElementById("manage-birthdays").addEventListener("click", toggleModal);
+document.getElementById("manage-birthdays").addEventListener("click", toggleModal)
+
 //Close modal
-document.querySelector(".close-button").addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+document.querySelector(".close-button").addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick)
 
+
+document.getElementById('difference-in-days').addEventListener('change', () => {
+    let DifferenceInDays = document.getElementById('difference-in-days')
+    setDifferenceInDays(DifferenceInDays.value)
+})
+
+
+// get dirthday
 document.getElementById('save-birthday').addEventListener('click', getBirthday)
-
-// clearStorage();
